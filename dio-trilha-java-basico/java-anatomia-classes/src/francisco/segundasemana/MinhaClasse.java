68b85f13c2503749d275cf763f1d738cb45b9c71
Francisco-Gorgonho <francisco.gorgonho@gmail.com>
2023-08-25 07:35:23 -0300

    first commit

---
+++ dio-trilha-java-basico/java-anatomia-classes/src/francisco/segundasemana/MinhaClasse.java
@@ -0,0 +1,20 @@
+package edu.francisco.segundasemana;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Dante";
+        String segundoNome = "Medeiros";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
+
